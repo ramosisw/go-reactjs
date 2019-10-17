ff06ddecb36b1c6f031273a60b793646b98ca8f6
julio.ramos <julio.ramos@diginteligencia.com>
2019-10-17 00:04:49 -0500

    Fix build

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 	"strings"
 
 	"github.com/ramosisw/todo-go-reactjs/frontend"
-	"github.com/ramosisw/todo-go-reactjs/handler"
+	"github.com/ramosisw/todo-go-reactjs/handlers"
 	"github.com/ramosisw/todo-go-reactjs/models"
 
 	assetfs "github.com/elazarl/go-bindata-assetfs"
